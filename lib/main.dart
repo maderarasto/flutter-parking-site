29e52e71f43f644b5af826b4c5c228f2fdd73673
Rastislav Maděra <maderarasto@gmail.com>
2021-06-08 20:47:20 +0200

    Initializes a base flutter project.

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ParkingSite());
+
+class ParkingSite extends StatelessWidget {
+  Widget build(BuildContext content) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
